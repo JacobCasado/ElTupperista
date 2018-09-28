601ba76717f817b58eb393ffc0b398299b4af500
agdwm <almudenaguerraswm@gmail.com>
2018-09-28 11:13:59 +0200

    Added the functionality that shows the user address instead the address form depending on the user has an address

--- public/javascripts/showTupperForm.js
+++ public/javascripts/showTupperForm.js
@@ -1,12 +1,12 @@
 $(document).ready(() => {
 	$('#isCookerTrue').on('click', () => {
 		if($(this).prop("checked", true)){
-			$('#form-tupper').stop().slideDown("slow");
+			$('#form-tupper').stop().slideDown("speed");
 		}
 	})
 	$('#isCookerFalse').on('click', () => {
 		if($(this).prop("checked", true)){
-			$('#form-tupper').stop().slideUp("slow");
+			$('#form-tupper').stop().slideUp("speed");
 		}
 	}) 
 });
