759ab79daadab3a946023a9f50a981019ad6cfa9
Jacob Casado <jacob.casado.perez@gmail.com>
2018-10-23 16:14:03 +0200

    mapas arreglados

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -4,8 +4,8 @@ const autocompleteInput = () => {
 	
 	google.maps.event.addListener(autocomplete, 'place_changed', function(){
 		var place = autocomplete.getPlace();
-		var lng = place.geometry.viewport.b.b;
-		var lat = place.geometry.viewport.f.f;
+		var lng = place.geometry.viewport.j.j;
+		var lat = place.geometry.viewport.l.l;
 		let latlng = { lat: lat, lng: lng };
 
 		setPosOnForm(latlng);
